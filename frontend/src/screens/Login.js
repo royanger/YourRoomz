95f84467063923b037d3825f5d77bd11e2dcf8b0
basit-dev <bas677@icloud.com>
2021-06-12 13:41:09 +0100

    Modified Login layout and added styles

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,9 +1,20 @@
-import * as React from 'react'
+import * as React from 'react';
+import Header from '../components/header/Header'
 
 const Login = () => {
   return (
     <>
-      <div>Login Screen</div>
+      <Header />
+        <div className="login">
+          <div className="login__intro">
+            <h1>Get deco recommendations for free</h1>
+            <p>Sign into your account</p>
+              <button className="login__btn">Sign in with Google</button>
+              <button className="login__btn">Sign in with Facebook</button>
+              <button className="login__btn">Sign in with Twitter</button>
+              <button className="login__btn">Not signed up? Sign up</button>
+          </div>  
+        </div>
     </>
   )
 }
