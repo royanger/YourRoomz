db3060b18474c259540fc5ebc66a545d3c64b0a6
Basit-Dev <63122554+Basit-Dev@users.noreply.github.com>
2021-06-18 00:31:29 +0100

    Updated room details and added styles

--- frontend/src/screens/RoomDetails.js
+++ frontend/src/screens/RoomDetails.js
@@ -1,10 +1,48 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import IntroText from '../components/IntroText'
+import '../styles/_room-details.scss'
 
 const RoomDetails = () => {
   return (
     <>
-      <div>RoomDetails screen</div>
+      <div className="container room-details">
+        <IntroText
+          variant="heading"
+          heading="What is your living room color?"
+          paragraph="Choose your living room wall color and floor color"
+        />
+        <section>
+          <div className="wall-color">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="c-picker">wall color picker</div>
+        </section>
+        <section>
+          <div className="floor-color">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="f-picker">floor color picker</div>
+        </section>
+      </div>
       <Link to="/room-specs">Back</Link>
       <Link to="/add-items">Next</Link>
     </>
