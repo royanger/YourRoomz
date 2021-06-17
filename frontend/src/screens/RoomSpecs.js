2f34504a27c650a931be9783ae0c3ee73cb40c6f
Basit-Dev <63122554+Basit-Dev@users.noreply.github.com>
2021-06-17 06:20:29 +0100

    Updated RoomSpecs, created input form and styles

--- frontend/src/screens/RoomSpecs.js
+++ frontend/src/screens/RoomSpecs.js
@@ -1,12 +1,20 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import IntroText from '../components/IntroText'
+import InputField from '../components/InputField'
 
 const RoomSpecs = () => {
   return (
     <>
-      <div>Room Specs screen</div>
-      <Link to="/add-room">Back</Link>
-      <Link to="/room-details">Next</Link>
+      <div className="container room-specs">
+        <IntroText
+          heading="What is your living room dimesion?" 
+          paragraph="Enter your living room dimensions in feeet and inches"
+        />
+        <InputField />
+        <Link to="/add-room">Back</Link>
+        <Link to="/room-details">Next</Link>
+      </div>
     </>
   )
 }
