f18277a32e45badcdcc114b485d6587de8e3c72b
basit-dev <bas677@icloud.com>
2021-06-20 18:29:37 +0100

    Removed default className

--- frontend/src/components/HeroCard.js
+++ frontend/src/components/HeroCard.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
-const HeroCard = ({ text, classes, variant, to, callBack }) => {
+const HeroCard = ({ text, variant, to, callBack }) => {
   return (
     <>
       <Link to={to}>
-        <div className={`herocard ${variant}`}>{text}</div>
+        <div className={variant}>{text}</div>
       </Link>
     </>
   )
