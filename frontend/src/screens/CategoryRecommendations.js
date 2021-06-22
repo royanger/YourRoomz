b4ae5330f00f1f0aecafa40b6e4bae7637c9b47a
basit-dev <bas677@icloud.com>
2021-06-23 00:23:13 +0100

    Updated category recommendations and styles

--- frontend/src/screens/CategoryRecommendations.js
+++ frontend/src/screens/CategoryRecommendations.js
@@ -1,10 +1,31 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import IntroText from '../components/IntroText'
+import HeroCard from '../components/HeroCard'
 
 const CategoryRecommendations = () => {
   return (
     <>
-      <div>CategoryRecommendations</div>
+      <div className="container-recommend">
+        <IntroText
+          variant="text"
+          heading="What category of items do you want us to recommend?"
+          paragraph="Choose the category of you want us to recommend"
+        />
+        <div className="main">
+          <div className="grid">
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+            <HeroCard to="/" text="Category" variant="category" />
+          </div>
+        </div>
+      </div>
       <Link to="/add-details">Back</Link>
       <Link to="/loading">Next</Link>
     </>
