dbc3d78dd94f7e8ec08922a13f1515d732ba04cc
Roy Anger <roy@royanger.com>
2021-07-04 00:29:33 -0400

    Removed Button import from Landing

--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../components/header/Header'
-import Button from '../components/Button'
 
 const Landing = () => {
   return (
