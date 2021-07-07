46b11270579a2b0672084187c07afa48fb24a513
Roy Anger <roy@royanger.com>
2021-07-07 02:21:54 -0400

    Updates to landing page design

--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -10,7 +10,7 @@ const Landing = () => {
         <div className="container">
           <div>
             <h2>Get recommendations for free</h2>
-            <h1>We Create Your Dream Room.</h1>
+            <h1>We Help You Create Your Dream Room.</h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisc elit. Nam nisl
               placerat vitae sit tellus. Amet ac sed porttitor eget interdum.
